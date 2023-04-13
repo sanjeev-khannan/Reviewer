e9c74f8103b925c8e6533c0772489f78542038ed
Sanjeev Kumar Srinivasan Khannan <sanjeevkumar.srinivasankhannan@pace.edu>
2023-04-13 17:50:54 -0400

    Updated changes related to DAO move refactor

--- src/main/java/com/reviewer/repositories/UserDetailsRepository.java
+++ src/main/java/com/reviewer/repositories/UserDetailsRepository.java
@@ -3,7 +3,7 @@ package com.reviewer.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.reviewer.pojos.User;
+import com.reviewer.dao.User;
 
 @Repository
 public interface UserDetailsRepository extends JpaRepository<User, Long> {

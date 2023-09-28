589f61695b8abd8feb79ede17ffaafee509b5878
Sanjeev Kumar Srinivasan Khannan <114128287+sanjeev-khannan@users.noreply.github.com>
2023-09-28 02:22:25 -0400

    Update ReviewerApplicationBoot.java

--- src/main/java/com/reviewer/ReviewerApplicationBoot.java
+++ src/main/java/com/reviewer/ReviewerApplicationBoot.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReviewerApplicationBoot {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReviewerApplicationBoot.class, args);
+        SpringApplication.run(ReviewerApplicationBoot.class, new String[] {});
+        
     }
 
 }

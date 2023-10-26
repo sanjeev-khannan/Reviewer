5b8bc9e8fc1f2991fff1a587f3143540ea3ecc35
sanjeev-khannan <naan.sanjeevkumar@gmail.com>
2023-10-25 23:29:39 -0400

    Fixed imports and indendations

--- src/main/java/com/reviewer/controllers/VenueController.java
+++ src/main/java/com/reviewer/controllers/VenueController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reviewer.dao.Venue;
@@ -16,12 +15,12 @@ import com.reviewer.services.VenueService;
 
 @RestController
 public class VenueController {
- 
-    @Autowired
-    public VenueService venueService;
 
-	@RequestMapping(value = "/venue", method = RequestMethod.POST)
-    public ResponseEntity<List<Venue>> searchVenue(@RequestBody SearchQuery searchQuery) throws Exception{
-		return ResponseEntity.ok(venueService.findVenueWithCriteria(searchQuery));
-    }
+  @Autowired
+  public VenueService venueService;
+
+  @RequestMapping(value = "/venue", method = RequestMethod.POST)
+  public ResponseEntity<List<Venue>> searchVenue(@RequestBody SearchQuery searchQuery) throws Exception {
+    return ResponseEntity.ok(venueService.findVenueWithCriteria(searchQuery));
+  }
 }
